f801cf818d0e31a5db03c30d74e058fa834a2ed9
erlighten <erlighten@gmail.com>
2020-02-04 20:34:23 +0600

    задачи

--- Program.cs
+++ Program.cs
@@ -8,9 +8,69 @@ namespace Lesson01_1
 {
     class Program
     {
-        int number = 1;
+        
         static void Main(string[] args)
         {
+            /*double num1 = 0;
+            double num2 = 0;
+            double result = 0;
+
+            Console.WriteLine("Введите первое число");
+            string input = Console.ReadLine();
+            num1 = Convert.ToInt32(input);
+            Console.WriteLine("Введите второе число");
+            input = Console.ReadLine();
+            num2 = Convert.ToInt32(input);
+            Console.WriteLine("Введите оператор + / - *");
+            input = Console.ReadLine();
+            if(input == "+")
+            {
+                result = num1 + num2;
+            }
+            else if(input == "-")
+            {
+                result = num1 - num2;
+            }
+            else if (input == "/")
+            {
+                result = num1 / num2;
+            }
+            else if (input == "*")
+            {
+                result = num1 * num2;
+            }
+            Console.WriteLine("Результат = " + result);*/
+
+            //string text = Console.ReadLine();
+            int number = 10;
+            if(number > 0)
+            {
+                Console.WriteLine("number > 0");
+            }
+            if(number > 5)
+            {
+                Console.WriteLine("number > 5");
+            }
+            if (number > 8)
+            {
+                Console.WriteLine("number > 8");
+            }
+
+            //Console.WriteLine(num1);
+
+            Console.ReadLine();
+
+
+
+            /*string text1 = "Text1";
+            string text2 = "Text2";
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            string c = "a = ";
+            Console.WriteLine(c + a + b);
+            Console.WriteLine(text1 + text2);
+            Console.ReadLine();*/
         }
     }
 }
